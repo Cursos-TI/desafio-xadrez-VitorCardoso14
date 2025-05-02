6ff16a43a4ecbd98f34377e4f540fd060d7ee9a3
VitorCardoso14 <vitorcardoso1401@gmail.com>
2025-05-02 21:20:00 +0000

    adicionei o do while

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    int moveCavalo;
+    int moveCavalo, i = 1;
 
 //usando while para Torre se mover
     printf("Bispo:\n");
@@ -32,15 +32,21 @@ int main() {
 
 
      printf("Mova o cavalo:\n");
-     printf("1.Direita.\n");
-     printf("2.Esquerda.\n");
+     printf("1.Cima, Cima e Direita.\n");
+     printf("2.Cima, Cima e Esquerda.\n");
+     printf("3.Baixo, Baixo e Direita.\n");
+     printf("4.Baixo, Baixo e Esquerda.\n");
+     printf("5.Direita, Direita e Cima.\n");
+     printf("6.Direita, Direita e Baixo.\n");
+     printf("7.Esquerda, Esquerda e Cima.\n");
+     printf("8.Esquerda, Esquerda e Baixo.\n");
      scanf("%d", &moveCavalo);
 
 
 switch (moveCavalo)
 {
 case 1:
-    //usando loop aninhado para mover o cavalo
+    //usando loop aninhado for para mover o cavalo
     printf("Cavalo:\n");
     for (int i = 1; i <= 1; i++)
     {
@@ -52,7 +58,7 @@ case 1:
     }
     break;
 case 2:
-    //usando loop aninhado para mover o cavalo
+    //usando loop aninhado for para mover o cavalo
     printf("Cavalo:\n");
     for (int i = 1; i <= 1; i++)
     {
@@ -62,6 +68,94 @@ case 2:
         }
         printf("Esquerda\n");
     }
+    break;
+case 3:
+    //usando loop aninhado while para mover o cavalo
+    printf("Cavalo:\n");
+    
+    while (i <= 1)
+    {int j = 1;
+        while (j <= 2)
+        {
+        printf("Baixo\n"); ++j; 
+        }
+        printf("Direita\n"); ++i;
+    }
+    break;
+    case 4:
+    //usando loop aninhado while para mover o cavalo
+    printf("Cavalo:\n");
+    
+    while (i <= 1)
+    {int j = 1;
+        while (j <= 2)
+        {
+        printf("Baixo\n");
+        ++j; 
+        }
+        printf("Esquerda\n");
+        ++i;
+    }
+    break;
+    case 5:
+    //usando loop aninhado do while para mover o cavalo
+    printf("Cavalo:\n");
+
+    do
+    {int j = 1;
+        do
+        {
+        printf("Direita\n");
+        ++j; 
+        } while (j <= 2);
+        printf("Cima\n");
+        ++i;
+    } while (i <= 1);
+    break;
+    case 6:
+    //usando loop aninhado do while para mover o cavalo
+    printf("Cavalo:\n");
+
+    do
+    {int j = 1;
+        do
+        {
+        printf("Direita\n");
+        ++j; 
+        } while (j <= 2);
+        printf("Baixo\n");
+        ++i;
+    } while (i <= 1);
+    break;
+    case 7:
+    //usando loop aninhado do while para mover o cavalo
+    printf("Cavalo:\n");
+
+    do
+    {int j = 1;
+        do
+        {
+        printf("Esquerda\n");
+        ++j; 
+        } while (j <= 2);
+        printf("Cima\n");
+        ++i;
+    } while (i <= 1);
+    break;
+    case 8:
+    //usando loop aninhado do while para mover o cavalo
+    printf("Cavalo:\n");
+
+    do
+    {int j = 1;
+        do
+        {
+        printf("Esquerda\n");
+        ++j; 
+        } while (j <= 2);
+        printf("Baixo\n");
+        ++i;
+    } while (i <= 1);
     break;
 default:
     printf("Movimento Invalido\n");
